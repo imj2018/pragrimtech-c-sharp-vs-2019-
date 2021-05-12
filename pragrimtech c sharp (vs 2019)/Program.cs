7efa8904d46ad6258dfbfc16019023e336d674f3
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-12 16:39:59 +1000

    Part 3 - Built-in types in C#

---
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pragrimtech_c_sharp__vs_2019_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
