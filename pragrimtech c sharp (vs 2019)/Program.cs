327f3cb01f90d373cbdc44bae01d3f10aa08ea00
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-14 08:13:32 +1000

    part 9 comments in c#

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -6,23 +6,15 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         static void Main(string[] args)
         {
-            string numbertwo = "2";
-            int result;
-            int.TryParse(numbertwo, out result);
-
-            int[] EvenNumbers = new int[3];
-            int[] OddNumbers = { 1, 3, 5, };
-
-            EvenNumbers[0] = 0;
-            EvenNumbers[1] = result;
-            EvenNumbers[2] = 4;
-            //EvenNumbers[3] = 6; // can't grow in size over time/once initialized
-
-            Console.WriteLine(EvenNumbers[1]);
-            //Console.WriteLine(EvenNumbers[10]);
-
-
-
+            SampleClass SampleClass;
         }
+    }
+
+    /// <summary>
+    /// This is a sample class and a sample documentation
+    /// </summary>
+    public class SampleClass
+    {
+
     }
 }
