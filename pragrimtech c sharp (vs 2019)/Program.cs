2a48cf5c8192cc2a595cf181494d52021f204745
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-18 07:53:31 +1000

    Part 17 - C# Tutorial - Method parameters.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -6,36 +6,29 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         public static void Main()
         {
-            Program Program = new Program();
-            //program.EvenNumbers();
+            int i = 0;
+            //SimpleMethod(i); // pass by value, pointing to different memery location, 0 is copied into variable
+            SimpleMethod(ref i); // i and j are pointing to same memory location, though i is set to 0 j is setting it to 101
 
-            Program.EvenNumbers(30);
+            Console.WriteLine(i);
 
-            int Sum = Program.Add(3, 4);
-            Console.WriteLine("Sum = {0}", Sum);
+            int Total = 0;
+            int Product = 0;
+            Calculate(10, 20, out Total, out Product);
+
+            Console.WriteLine("Sum = {0} and Product = {1}", Total, Product);
         }
 
-        public int Add(int FN, int SN)
+        //public static void SimpleMethod(int j)
+        public static void SimpleMethod(ref int j)
         {
-            return FN + SN;
+            j = 101;
         }
 
-        //public void EvenNumbers() // instance method 
-        public static void EvenNumbers(int Target) // static method
+        public static void Calculate(int fn, int sn, out int Sum, out int Product)
         {
-            int Start = 0;
-            while (Start < Target)
-            {
-                if (Start % 2 == 1)
-                {
-                    continue;
-                }
-                //Console.WriteLine(Start);
-                //Start = Start + 2;
-                Start += 2;
-                Console.WriteLine(Start);
-            }
-
+            Sum = fn + sn;
+            Product = fn * sn;
         }
     }
 }
