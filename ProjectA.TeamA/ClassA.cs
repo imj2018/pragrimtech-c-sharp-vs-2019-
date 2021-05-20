007e5348d555840e0b66e97c79e17606263e1ae2
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-20 16:59:16 +1000

    Part 18 - C# Tutorial - Namespaces.avi

---
+++ ProjectA.TeamA/ClassA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//namespace ProjectA.TeamA
+namespace ProjectA {
+
+    // can nest namespaces
+    namespace TeamA
+    {
+        public class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A Print Method");
+            }
+        }
+    }
+
+}
--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+//using PATA = ProjectA.TeamA; // project alias
+//using PATB = ProjectA.TeamB;
+using ProjectA.TeamA;
+using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
@@ -6,47 +10,21 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         public static void Main()
         {
-            int Total = 0;
-            int Product = 0;
-            Calculate(10, 20, out Total, out Product);
+            Console.WriteLine("Hello");
 
-            Console.WriteLine("Sum = {0} and Product - {1}", Total, Product);
+            //ClassA.Print();
+            //ProjectA.TeamB.ClassA.Print();
+            //ProjectA.TeamA.ClassA.Print();
+
+            // namespace readability,
+            //PATA.ClassA.Print();
+            //PATB.ClassA.Print();
+
+            ProjectA.TeamA.ClassA.Print();
 
 
-            int[] Numbers = new int[3]
-            {
-                101,
-                102,
-                103
-            };
-
-            ParamsMethod();
-            ParamsMethod(Numbers);         
-            ParamsMethod(1, 2, 3, 4, 5); 
 
         }
-
-        // output parameters when returning multiple values
-        public static void Calculate(int FN, int SN, out int Sum, out int Product) 
-        {
-            Sum = FN + SN;
-            Product = FN * SN;
-        }
-
-        //public static void ParamsMethod(int[] Numbers)
-        // params keyword makes parameter optional
-        // params can pass element values
-        // must be the last parameter e.g ParamsMethod(int X, params int[] Numbers)
-        // cannot have multiple ParamsMethod(params int[] Numbers, params string[] Names)
-        public static void ParamsMethod(params int[] Numbers)
-        {
-            Console.WriteLine("There are {0} elements", Numbers.Length);
-            foreach (var item in Numbers)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
 
     }
 }
