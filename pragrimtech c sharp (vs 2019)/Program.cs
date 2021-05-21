bcfbf126435cda0563627d6315f18d92358f1e5d
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-21 15:42:46 +1000

    Part 19 - C# Tutorial - Introduction to classes.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 //using PATA = ProjectA.TeamA; // project alias
 //using PATB = ProjectA.TeamB;
 using ProjectA.TeamA;
@@ -6,25 +7,52 @@ using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
+    class Customer
+    {
+        public string _firstName;
+        public string _lastName;
+
+        // default constructor that can be useful for no parameters
+        public Customer() : this("No FirstName Provided", "No LastName Provided")
+        {
+
+        }
+
+        // .net has a default constructor with no parameters (if no constructor is declared)
+        // overloading constructors
+        public Customer(string FirstName, string LastName)
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine("Full Name = {0}", this._firstName + " " + this._lastName);
+        }
+
+        // destructor, to manually clean up resources as opposed to garbage collector
+        ~Customer()
+        {
+
+        }
+    }
     class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Hello");
+            //Customer C1 = new Customer("Pragim", "Technologies");
+            Customer C1 = new Customer();
+            C1.PrintFullName();
 
-            //ClassA.Print();
-            //ProjectA.TeamB.ClassA.Print();
-            //ProjectA.TeamA.ClassA.Print();
-
-            // namespace readability,
-            //PATA.ClassA.Print();
-            //PATB.ClassA.Print();
-
-            ProjectA.TeamA.ClassA.Print();
-
-
+            Customer C2 = new Customer("Joe", "Smith");
+            C2.PrintFullName();
 
         }
 
     }
-}
+
+ }
+
+
+
