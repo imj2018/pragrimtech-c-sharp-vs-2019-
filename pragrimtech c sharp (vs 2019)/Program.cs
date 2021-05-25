819135511eb957a0f1d46f4174b2792dcd9c97d4
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-25 14:16:23 +1000

    Part 22 - C# Tutorial - Method hiding in c#.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -6,35 +6,8 @@ using ProjectA.TeamA;
 using ProjectA.TeamB;
 
 
-
 namespace pragrimtech_c_sharp__vs_2019_
 {
-
-    // when a child/derived object is created a base class instance is created first
-    // the constructor of the base class is called first
-    // e.g PrintFullName() is an instance member (not static) of the base class 
-    public class ParentClass
-    {
-        public ParentClass()
-        {
-            Console.WriteLine("ParentClass Constructor called");
-        }
-
-        public ParentClass(string Message)
-        {
-            Console.WriteLine(Message);
-        }
-    }
-
-    public class ChildClass : ParentClass
-    {
-        // to choose the constructor from the parent the base keyword can be used
-        // intellisense will show whih constructor to choose from
-        public ChildClass() : base("Derived class controlling Parent class constructor called")
-        {
-            Console.WriteLine("ChildClass Constructor called");
-        }
-    }
 
     public class Employee
     {
@@ -46,52 +19,45 @@ namespace pragrimtech_c_sharp__vs_2019_
         {
             Console.WriteLine(FirstName + " " + LastName);
         }
-
     }
 
-    // analogy like derived class is comparitive to a general surgeon to specialized doctor,
-    // at times can e.g a gynecologist can do there gynocology things but also perform general surgery  
     public class FullTimeEmployee : Employee
     {
-        public float YearlySalary;
+        // this will hide the parent method with the same name
+        // this will be invoked not the parent
+        // use the new keyword if hiding is intentional
+        public new void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " Contractor");
+            // call the base class method
+            //base.PrintFullName();
+        }
     }
 
-    // only single class inheritance allowed
-    //public class PartTimeEmployee : Employee, A
     public class PartTimeEmployee : Employee
     {
-        public float HourlyRate;
+        //
     }
 
-    // multilevel class inheritance
-    // class A will have the code from PartTimeEmployee and Employee
-    public class A : PartTimeEmployee
-    {
-
-    }
-
-    class Program
+    public class Program
     {
         public static void Main()
         {
+            //FullTimeEmployee FTE = new FullTimeEmployee();
+            // assign the child to a reference of parent, this will now call the PrintFullName of the parent
+            // not the PartTimeEmployee method
+            Employee FTE = new FullTimeEmployee();
+            FTE.FirstName = "mr full time";
+            FTE.LastName = "employee";
 
-            FullTimeEmployee FTE = new FullTimeEmployee();
-            FTE.FirstName = "Pragim";
-            FTE.LastName = "Tech";
-            FTE.PrintFullName();
-            FTE.YearlySalary = 50000;
+            // type case to convert to Employee 
+            //((Employee)FTE).PrintFullName();
+
 
             PartTimeEmployee PTE = new PartTimeEmployee();
-            PTE.FirstName = "Partime";
-            PTE.LastName = "Guy";
+            PTE.FirstName = "mr part time";
+            PTE.LastName = "employee";
             PTE.PrintFullName();
-            PTE.HourlyRate = 40;
-
-            A A1 = new A();
-            A1.FirstName = "multi";
-            A1.HourlyRate = 40;
-
-            ChildClass CC = new ChildClass();
 
         }
     }
