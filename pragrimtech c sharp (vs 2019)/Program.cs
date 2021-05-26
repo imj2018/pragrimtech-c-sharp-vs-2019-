5c4c55f3e51d2e0036e282a838990188993c332e
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-27 07:58:47 +1000

    Part 26 - C# Tutorial - Why Properties.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,58 +8,85 @@ using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
+    public class Student
+    {
+        // encapsulation
+        private int _id;
+        private string _name;
+
+        // as there is only a get method/return it is essentially readonly
+        private int _passMark = 35;
+
+        public int GetPassMark()
+        {
+            return this._passMark;
+        }
+
+        public void SetName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new Exception("Name cannot be null or empty");
+            }
+            else
+            {
+                this._name = Name;
+            }
+        }
+
+        public string GetName()
+        {
+            return string.IsNullOrEmpty(this._name) ? "No Name" : this._name;
+
+            //if (string.IsNullOrEmpty(this._name))
+            //{
+            //    return "No Name";
+            //}
+            //else
+            //{
+            //    return this._name;
+            //}
+        }
+
+        public int GetId()
+        {
+            return this._id;
+        }
+
+        public void SetId(int Id)
+        {
+            if (Id <= 0)
+            {
+                throw new Exception("Student Id cannot be negative");
+            }
+            else
+            {
+                this._id = Id;
+            }
+        }
+
+    }
 
     public class Program
     {
         public static void Main()
         {
-            int Sum;
-            Add(2, 3, out Sum);
+            Student S1 = new Student()
+            {
+
+            };
+
+            S1.SetId(101);
+
+            //Console.WriteLine("ID = {0} && Name = {1} PassMark = {2}",
+            //    S1.ID, S1.Name, S1.PassMark);
+
+            Console.WriteLine("Student Id = {0}", S1.GetId());
+            Console.WriteLine("Student Name = {0}", S1.GetName());
+            Console.WriteLine("Student Pass Mark = {0}", S1.GetPassMark());
+
+
         }
-
-        public static void Add(int FN, int SN) // the signature is Add(int FN, int SN)
-        {
-            Console.WriteLine("Sum = {0}", FN + SN);
-        }
-
-        // can also be overloaded based on type of parameter
-        public static void Add(float FN, int SN)
-        {
-            Console.WriteLine("Sum = {0}", FN + SN);
-        }
-
-        // params keyword does not count towards overloading and will throw an error
-        public static void Add(float FN, int SN, params int[] TN)
-        {
-            Console.WriteLine("Sum = {0}", FN + SN);
-        }
-
-        //public static void Add(float FN, int SN,int[] TN)
-        //{
-        //    Console.WriteLine("Sum = {0}", FN + SN);
-        //}
-
-        // not possible to overload a function based on the type (written type) i.e return type int
-        //public static int Add(float FN, int SN)
-        //{
-        //    Console.WriteLine("Sum = {0}", FN + SN);
-        //    return FN + SN;
-        //}
-
-        // out will have a return value
-        // to reiterate the paramter types are value, ref, output, params arrays
- 
-        public static void Add(int FN, int SN, int TN) // overloading
-        {
-            Console.WriteLine("Sum = {0}", FN + SN + TN);
-        }
-
-        public static void Add(int FN, int SN, int TN, int fourth) 
-        {
-            Console.WriteLine("Sum = {0}", FN + SN + TN + fourth);
-        }
-
-
     }
 
 }
