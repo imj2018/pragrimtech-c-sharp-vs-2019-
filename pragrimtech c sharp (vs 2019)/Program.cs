dd31a3cdd84078eb4d6c77c4d23b9d1e69b52400
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-05-26 16:27:23 +1000

    Part 25 - C# Tutorial - Method overloading in c#.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -9,38 +9,59 @@ using ProjectA.TeamB;
 namespace pragrimtech_c_sharp__vs_2019_
 {
 
-    public class BaseClass
-    {
-        public virtual void Print()
-        {
-            Console.WriteLine("I am a Base Class Print Method");
-        }
-    }
-
-    public class DerivedClass : BaseClass
-    {
-        //public override void Print()
-        public new void Print() // hiding
-        {
-            Console.WriteLine("I am a Derived Class Print Method");
-        }
-    }
-
     public class Program
     {
         public static void Main()
         {
-            // to reiterate a base class reference variable points to a derived class object
-            // if  a method is overriden in the derived class it will be invoked instead of the base class
-            BaseClass B = new DerivedClass();
-            B.Print();
-
-            DerivedClass D = new DerivedClass();
-            D.Print();
-
-
+            int Sum;
+            Add(2, 3, out Sum);
         }
+
+        public static void Add(int FN, int SN) // the signature is Add(int FN, int SN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+
+        // can also be overloaded based on type of parameter
+        public static void Add(float FN, int SN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+
+        // params keyword does not count towards overloading and will throw an error
+        public static void Add(float FN, int SN, params int[] TN)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+
+        //public static void Add(float FN, int SN,int[] TN)
+        //{
+        //    Console.WriteLine("Sum = {0}", FN + SN);
+        //}
+
+        // not possible to overload a function based on the type (written type) i.e return type int
+        //public static int Add(float FN, int SN)
+        //{
+        //    Console.WriteLine("Sum = {0}", FN + SN);
+        //    return FN + SN;
+        //}
+
+        // out will have a return value
+        // to reiterate the paramter types are value, ref, output, params arrays
+ 
+        public static void Add(int FN, int SN, int TN) // overloading
+        {
+            Console.WriteLine("Sum = {0}", FN + SN + TN);
+        }
+
+        public static void Add(int FN, int SN, int TN, int fourth) 
+        {
+            Console.WriteLine("Sum = {0}", FN + SN + TN + fourth);
+        }
+
+
     }
+
 }
 
 
