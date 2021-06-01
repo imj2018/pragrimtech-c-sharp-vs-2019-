733d192c9028ad90e829403eab5221621207b77b
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-06-01 17:16:17 +1000

    Part 35 - C# Tutorial - Multiple class inheritance using interfaces.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -9,55 +9,66 @@ using System.Linq;
 namespace pragrimtech_c_sharp__vs_2019_
 {
     /// <summary>
-    // class A is super base class
-    // both class B and C are overriding the virtual method from A
-    // if class D could inherit from B and C and does not override the Print method which
-    // should it invoke from B or C? i.e the Diamond problem
+    // class A inherits from IA, class B inherits from IB
+    // class AB should have the capability of A and B i.e what multiple inheritance tries to achieve
+    // C#/.net will not allow inheritance from multiple classes AB : A, B
     /// </summary>
-    class A
+    interface IA
     {
-        public virtual void Print()
+        void AMethod();
+    }
+
+    class A : IA
+    {
+        public void AMethod()
         {
-            Console.WriteLine("A implementation");
+            Console.WriteLine("A");
+        }
+    }
+    interface IB
+    {
+        void BMethod();
+    }
+
+    class B : IB
+    {
+        public void BMethod()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    //class AB : A, B
+    class AB : IA, IB
+    {
+
+        A a = new A();
+        B b = new B();
+
+        public void AMethod()
+        {
+            // as class A and B already have implementation the methods can be invoked
+            a.AMethod();
+        }
+
+        public void BMethod()
+        {
+            b.BMethod();
         }
 
     }
 
-    class B : A
-    {
-        public override void Print()
-        {
-            //base.Print();
-            Console.WriteLine("B Implementation");
-        }
-
-    }
-
-    class C : A
-    {
-        public override void Print()
-        {
-            Console.WriteLine("C Implementation");
-        }
-    }
-
-    class D : B, C
-    {
-
-    }
 
     public class Program
-    { 
+    {
+        // has the capability of class A and B
         public static void Main()
         {
-            B b = new B();
-            b.Print();
+            AB ab = new AB();
+            ab.AMethod();
+            ab.BMethod();
 
-            D d = new D();
-            d.Print();
         }
-        
-
     }
 
 }
