82b0d6d9eeb95df152c32f89189492f5ef4a19a2
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-06-03 07:51:27 +1000

    Part 37 - C# Tutorial - Delegates usage in c# - 1

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,44 +8,42 @@ using System.Linq;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
 
-    /// <summary>
-    // "A delegate is a type safe function pointer"
-    // the syntax of a delegate is like a function
-    // a delegate keyword should be used, think of the syntax as method with the delegate keyword and similiar
-    // to a method signature
-    // this delegate can now be used to point to a function with a similar signature i.e a return type void
-    // and a string parameter
-    /// </summary>
-    public delegate void HelloFunctionDelegate(string message);
-    public delegate int NumberCalculationDelegate();
+        public static void PromoteEmployee(List<Employee> employeeList)
+        {
+            // what if company x wants to promote an employee under different
+            // business rules, we can't change this at is hard coded
+            foreach (Employee employee in employeeList)
+            {
+                if (employee.Experience >= 5)
+                {
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
+        }
+
+    }
+
 
     public class Program
     {
         public static void Main()
         {
-            // like a class an instance of delegate can be created then in the constructor pass
-            // the function the delegate should point to 
-            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5},
+                new Employee() { ID = 101, Name = "Mike", Salary = 4000, Experience = 4},
+                new Employee() { ID = 101, Name = "John", Salary = 6000, Experience = 5},
+                new Employee() { ID = 101, Name = "Todd", Salary = 3000, Experience = 3}
+            };
 
-            // to invoke the delegate, behind the scenes the Hello method will be called
-            // then pass the something for the string parameter
-            del("Hello from Delegate");
-
-            NumberCalculationDelegate nc = new NumberCalculationDelegate(NumberCalculation);
-            Console.WriteLine(nc());
-        }
-
-        // type safe, the delegate needs to match the signature it points to 
-        //public static string void Hello(string message)
-        public static void Hello(string message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public static int NumberCalculation()
-        {
-            return 1 + 1;
+            Employee.PromoteEmployee(employees);
         }
 
     }
