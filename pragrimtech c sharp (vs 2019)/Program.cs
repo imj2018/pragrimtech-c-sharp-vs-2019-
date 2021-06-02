041a00d82095523828515a6df66642948e2d0e9e
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-06-03 07:29:35 +1000

    Part 36 - C# Tutorial - Delegates in c#.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,68 +8,49 @@ using System.Linq;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
+
     /// <summary>
-    // class A inherits from IA, class B inherits from IB
-    // class AB should have the capability of A and B i.e what multiple inheritance tries to achieve
-    // C#/.net will not allow inheritance from multiple classes AB : A, B
+    // "A delegate is a type safe function pointer"
+    // the syntax of a delegate is like a function
+    // a delegate keyword should be used, think of the syntax as method with the delegate keyword and similiar
+    // to a method signature
+    // this delegate can now be used to point to a function with a similar signature i.e a return type void
+    // and a string parameter
     /// </summary>
-    interface IA
-    {
-        void AMethod();
-    }
-
-    class A : IA
-    {
-        public void AMethod()
-        {
-            Console.WriteLine("A");
-        }
-    }
-    interface IB
-    {
-        void BMethod();
-    }
-
-    class B : IB
-    {
-        public void BMethod()
-        {
-            Console.WriteLine("B");
-        }
-    }
-
-    //class AB : A, B
-    class AB : IA, IB
-    {
-
-        A a = new A();
-        B b = new B();
-
-        public void AMethod()
-        {
-            // as class A and B already have implementation the methods can be invoked
-            a.AMethod();
-        }
-
-        public void BMethod()
-        {
-            b.BMethod();
-        }
-
-    }
-
+    public delegate void HelloFunctionDelegate(string message);
+    public delegate int NumberCalculationDelegate();
 
     public class Program
     {
-        // has the capability of class A and B
         public static void Main()
         {
-            AB ab = new AB();
-            ab.AMethod();
-            ab.BMethod();
+            // like a class an instance of delegate can be created then in the constructor pass
+            // the function the delegate should point to 
+            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
 
+            // to invoke the delegate, behind the scenes the Hello method will be called
+            // then pass the something for the string parameter
+            del("Hello from Delegate");
+
+            NumberCalculationDelegate nc = new NumberCalculationDelegate(NumberCalculation);
+            Console.WriteLine(nc());
         }
+
+        // type safe, the delegate needs to match the signature it points to 
+        //public static string void Hello(string message)
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static int NumberCalculation()
+        {
+            return 1 + 1;
+        }
+
     }
+
+
 
 }
 
