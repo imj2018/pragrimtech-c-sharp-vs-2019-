efbe4d19bed6daa4acccc626a3e6560bfe706ec0
Issey Jordan <Issey.Jordan@hpw.qld.gov.au>
2021-06-07 07:40:47 +1000

    Part 37 - C# Tutorial - Delegates usage in c# -2

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,27 +8,38 @@ using System.Linq;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
-    class Employee
+
+    public class Employee
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int Salary { get; set; }
         public int Experience { get; set; }
 
-        public static void PromoteEmployee(List<Employee> employeeList)
+        // pass the delegate as a parameter, at runtime the delegate will invoke the method IsPromotable
+        // is pointing to
+        // essentially this function is taking a another function as a parameter (when you want
+        // to pass a function as a paramter think of delegates
+
+        public static void PromoteEmployee(List<Employee> employeeList, IsPromotable isPromotable)
         {
-            // what if company x wants to promote an employee under different
-            // business rules, we can't change this at is hard coded
             foreach (Employee employee in employeeList)
             {
-                if (employee.Experience >= 5)
+                // instead of hard coding the logic, it can be decoupled
+                // condition returns a bool and should match the delegate and will be replaced by
+                // a delegate
+                //if (employee.Experience >= 5)
+                // the logic to promote will now be decided by the end user
+                if (isPromotable(employee))
                 {
                     Console.WriteLine(employee.Name + " promoted");
                 }
             }
         }
-
     }
+
+    // delegate will return a true or false for an Employee passed in
+    public delegate bool IsPromotable(Employee employee);
 
 
     public class Program
@@ -38,13 +49,40 @@ namespace pragrimtech_c_sharp__vs_2019_
             List<Employee> employees = new List<Employee>()
             {
                 new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5},
-                new Employee() { ID = 101, Name = "Mike", Salary = 4000, Experience = 4},
-                new Employee() { ID = 101, Name = "John", Salary = 6000, Experience = 5},
-                new Employee() { ID = 101, Name = "Todd", Salary = 3000, Experience = 3}
+                new Employee() { ID = 102, Name = "Mike", Salary = 4000, Experience = 4},
+                new Employee() { ID = 103, Name = "John", Salary = 6000, Experience = 5},
+                new Employee() { ID = 104, Name = "Todd", Salary = 3000, Experience = 3}
             };
 
-            Employee.PromoteEmployee(employees);
+            // to reiterate a delegate is like a class, when an instance is created the constructor accepts the
+            // name of the function and the signature must match
+            // pass the function to the constructor of the delegate, no brackets Promote()
+            //IsPromotable isPromotable = new IsPromotable(Promote);
+
+            // this now takes another parameter which is the delegate
+            //Employee.PromoteEmployee(employees, isPromotable);
+
+            // delegates can add more difficulty
+            // a lambda can be used instead (lambda expressions are based on delegates) 
+            // it will operate on an employee object
+            // employee such that an employee experience >= 5
+            // behind the scenes it operates the same as a delegate
+            Employee.PromoteEmployee(employees, employee => employee.Experience >= 5);
         }
+
+
+        // the logic is now outside 
+        //public static bool Promote(Employee employee)
+        //{
+        //    if (employee.Experience >= 5)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
 
     }
 
