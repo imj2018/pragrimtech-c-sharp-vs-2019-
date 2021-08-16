9001b99f213bea2adaae124b6b2923cf15030cb3
imj <imj.programming.2013@gmail.com>
2021-08-16 19:46:16 +1000

    Part 33 - C# Tutorial - Difference between abstract classes and interfaces.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,35 +8,30 @@ using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
-    // cannot be instatiated, can only be a base class
-    // for other classes
-    public abstract class Customer
+    // abstract class can inherit from another abstract class or interface
+    public abstract class Customer : ICustomer
     {
-        //public abstract void Print();
         public void Print()
         {
             Console.WriteLine("Print");
-        }
-
+        } 
     }
 
-    public class Program : Customer
-    //public abstract class Program : Customer
+    // interface can only inherit form another interface
+    public interface ICustomer
     {
-        //public override void Print()
-        //{
-        //    Console.WriteLine("Print Method");
-        //}
+        // can't have implementation or fields
+        //int ID;
+
+        void Print();
+    }
+
+    public class Program
+    {
         public static void Main()
         {
-            //Program P = new Program();
-            // as an abstract parent class can point to derived class object
-            Customer C = new Program();
-            C.Print();
 
         }
-
-
     }
 }
 
