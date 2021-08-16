5c24320dc1f08ea5d914e3c8ce10692335940a40
imj <imj.programming.2013@gmail.com>
2021-08-16 20:20:52 +1000

    Part 36 - C# Tutorial - Delegates in c#.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,57 +8,24 @@ using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
-    interface IA
-    {
-        void AMethod();
-    }
 
-    class A : IA
-    {
-        public void AMethod()
-        {
-            Console.WriteLine("A");
-        }
-    }
-
-    interface IB
-    {
-        void BMethod();
-    }
-    class B: IB
-    {
-        public void BMethod()
-        {
-            Console.WriteLine("B");
-        }
-    }
-
-    //class AB : A, B
-    class AB : IA, IB
-    {
-        // instead of re-implementing use the implementation 
-        // from class A and B
-        A a = new A();
-        B b = new B();
-
-        public void AMethod()
-        {
-            a.AMethod();
-        }
-
-        public void BMethod()
-        {
-            b.BMethod();
-        }
-    }
-
+    public delegate void HelloFunctionDelegate(string message);
+ 
     public class Program
     {
         public static void Main()
         {
-            AB ab = new AB();
-            ab.AMethod();
-            ab.BMethod();
+            // A delegate is a type safe function pointer
+
+            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+            del("Hello from Delegate");
+
+        }
+
+        //public static string Hello(string message)
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
