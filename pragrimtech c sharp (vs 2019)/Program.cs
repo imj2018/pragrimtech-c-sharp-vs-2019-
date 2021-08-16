ec64121358083b010bfd5b062250339a1668abdc
imj <imj.programming.2013@gmail.com>
2021-08-17 09:47:09 +1000

    Part 40   C# Tutorial   Exception Handling in C#

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 //using PATB = ProjectA.TeamB;
 using ProjectA.TeamA;
 using ProjectA.TeamB;
+using System.IO;
 
 
 namespace pragrimtech_c_sharp__vs_2019_
@@ -13,75 +14,43 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         public static void Main()
         {
-            //SampleDelegate del = new SampleDelegate(SampleMethodOne);
-            //del();
 
-            //SampleDelegate del1, del2, del3, del4;
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = 
+                    new StreamReader(@"C:\Users\miral\source\repos\pragrimtech-c-sharp-vs-2019-\pragrimtech c sharp (vs 2019)\data1.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                //Console.WriteLine(ex.Message);
+                //Console.WriteLine();
+                //Console.WriteLine();
+                //Console.WriteLine(ex.StackTrace);
 
-            //del1 = new SampleDelegate(SampleMethodOne);
-            //del2 = new SampleDelegate(SampleMethodTwo);
-            //del3 = new SampleDelegate(SampleMethodThree);
+                // Log the details to the DB
+                Console.WriteLine("Please check if the file {0} exists", ex.FileName);
 
-            //// del4 is pointing to multiple functions i.e a multicast delegate 
-            //del4 = del1 + del2 + del3 - del2;
-            //del4();
-
-            // register these methods to this delegate, delegate invocation list
-            //SampleDelegate del = new SampleDelegate(SampleMethodOne);
-            //del += SampleMethodTwo;
-            //del += SampleMethodThree;
-            //del -= SampleMethodOne;
-
-            //del();
-
-            SampleDelegate del = new SampleDelegate(SampleMethodOne);
-            del += SampleMethodTwo;
-
-            // the last method in the invocation list will be returned
-            //int delegateReturnValue = del();
-            int delegateReturnOutputParameterValue = 0;
-            del(out delegateReturnOutputParameterValue);
-
-            Console.WriteLine(delegateReturnOutputParameterValue);
-
-
+            }
+            // an example of inheritance, practical application of inheritance, specific exceptions at the top
+            // general at the bottom
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                Console.WriteLine("Finally Block");
+            }
         }
 
-        //public static void SampleMethodOne()
-        //{
-        //    Console.WriteLine("SampleMethodOne Invoked");
-        //}
-        //public static void SampleMethodTwo()
-        //{
-        //    Console.WriteLine("SampleMethodTwo Invoked");
-        //}
-        //public static void SampleMethodThree()
-        //{
-        //    Console.WriteLine("SampleMethodThree Invoked");
 
-        //public delegate void SampleDelegate();
-        //public delegate int SampleDelegate();
-        public delegate void SampleDelegate(out int Number);
-
-        //public static int SampleMethodOne()
-        //{
-        //    return 1;
-        //}
-
-        public static void SampleMethodOne(out int Number)
-        {
-            Number = 1;
-        }
-
-        //public static int SampleMethodTwo()
-        //{
-        //    return 2;
-        //}
-
-        public static void SampleMethodTwo(out int Number)
-        {
-            Number = 2;
-        }
 
 
     }
