eee412667d84ad19f5619798be9a17a07985ad9d
imj <imj.programming.2013@gmail.com>
2021-08-16 11:05:45 +1000

    Part 29 - C# Tutorial - Difference between classes and structs in c#.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,6 +8,7 @@ using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
+    // structs are sealed, cannot inherit from struct
     public struct Customer
     {
         private int _id;
@@ -25,6 +26,8 @@ namespace pragrimtech_c_sharp__vs_2019_
             set { _name = value; }
         }
 
+        //public Customer() { }
+
         public Customer(int id, string name)
         {
             this._id = id;
@@ -33,9 +36,14 @@ namespace pragrimtech_c_sharp__vs_2019_
 
         public void PrintDetails()
         {
-            Console.WriteLine("Id = {0} && Name = {1}", this._id, this._name);
+            Console.WriteLine("Id = {0} && Name = {1}", 
+                this._id, this._name);
         }
 
+        //~Customer()
+        //{
+
+        //}
 
 
     }
@@ -44,33 +52,42 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         public static void Main()
         {
-            Customer C1 = new Customer(47, "agent");
-            C1.PrintDetails();
+            //int i = 10;
 
-            Customer C2 = new Customer();
-            C2.ID = 117;
-            C2.Name = "john";
-            C2.PrintDetails();
-
-            Customer C3 = new Customer()
-            {
-                ID = 103,
-                Name = "boba fett"
-            };
-            C3.PrintDetails();
-
-
-            // messing around
-            //Student[] students = new Student[2]
+            //if (i == 10)
             //{
-            //    new Student() { Name = "agent", Id = 47 },
-            //    new Student() { Name = "john", Id = 117 }
-            //};
-
-            //foreach (var item in students)
-            //{
-            //    Console.WriteLine("{0} {1}", item.Name, item.Id);
+            //    int j = 20;
+            //    Customer C1 = new Customer()
+            //    {
+            //        ID = 47,
+            //        Name = "agent"
+            //    };
+            //    Console.WriteLine("in the scope of if block {0}", C1.ID);
             //}
+            //Console.WriteLine("in the scope of Main");
+
+            int i = 10;
+
+            // j will copy the value, it will not affect i as they are two
+            // different copies
+            int j = i;
+            j = j + 1;
+
+            Console.WriteLine("i = {0} && j = {1}", i, j);
+
+            Customer C1 = new Customer()
+            {
+                ID = 47,
+                Name = "agent"
+            };
+
+            // referencing same object
+            Customer C2 = C1;
+            C2.Name = "john";
+
+            Console.WriteLine("C1.Name = {0} && C2.Name = {1}", 
+                C1.Name, C2.Name);
+
         }
     }
 }
