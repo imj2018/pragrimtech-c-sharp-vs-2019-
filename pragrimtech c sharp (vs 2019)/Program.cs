d3d8fa7d2ee83bff1c18a7feb4cc7229ba5126e2
imj <imj.programming.2013@gmail.com>
2021-08-16 21:10:37 +1000

    Part 37 - C# Tutorial - Delegates usage in c#.avi

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,25 +8,42 @@ using ProjectA.TeamB;
 
 namespace pragrimtech_c_sharp__vs_2019_
 {
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
 
-    public delegate void HelloFunctionDelegate(string message);
- 
+        public static void PromoteEmployee(List<Employee> employeeList)
+        {
+            foreach(Employee employee in employeeList)
+            {
+                if(employee.Experience >= 5)
+                {
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
+        }
+
+    }
+
     public class Program
     {
         public static void Main()
         {
-            // A delegate is a type safe function pointer
+            List<Employee> employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 101, Name = "Mary", Salary = 5000, Experience = 5},
+                new Employee() { Id = 102, Name = "Mike", Salary = 4000, Experience = 4},
+                new Employee() { Id = 103, Name = "John", Salary = 6000, Experience = 6},
+                new Employee() { Id = 104, Name = "Todd", Salary = 3000, Experience = 3}
+            };
 
-            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
-            del("Hello from Delegate");
+            Employee.PromoteEmployee(employeeList);
 
         }
 
-        //public static string Hello(string message)
-        public static void Hello(string message)
-        {
-            Console.WriteLine(message);
-        }
     }
 }
 
