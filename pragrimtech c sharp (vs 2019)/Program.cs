f5c992da56fb6841787232cd024978910bade289
imj <imj.programming.2013@gmail.com>
2021-08-17 13:53:23 +1000

    Part 46   C# Tutorial   Enums Example

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -16,9 +16,9 @@ namespace pragrimtech_c_sharp__vs_2019_
         {
             Customer[] customers = new Customer[3]
             {
-                new Customer() { Name = "Mark", Gender = 1},
-                new Customer() { Name = "Mary", Gender = 2 },
-                new Customer() { Name = "Sam", Gender = 0 }
+                new Customer() { Name = "Mark", Gender = Gender.Male},
+                new Customer() { Name = "Mary", Gender = Gender.Female },
+                new Customer() { Name = "Sam", Gender = Gender.Unkown }
             };
 
             for (int i = 0; i < customers.Length; i++)
@@ -30,28 +30,35 @@ namespace pragrimtech_c_sharp__vs_2019_
 
         }
 
-
     }
 
+    public enum Gender
+    {
+        Unkown,
+        Male,
+        Female
+    }
 
     public class Customer
     {
         public string Name { get; set; }
-        public int Gender { get; set; }
+        public Gender Gender { get; set; }
 
-        public static string GetGender(int gender)
+        public static string GetGender(Gender gender)
         {
             // 0 - Unkown
             // 1 - Male
             // 2 - Female
-            // less readable should the application grow, which number correspond to which number?
+            // less readable should the application grow, which number correspond to which number?)
             switch (gender)
             {
-                case 0:
+                //case 0:
+                //    return "Uknown";
+                case Gender.Unkown:
                     return "Unknown";
-                case 1:
+                case Gender.Male:
                     return "Male";
-                case 2:
+                case Gender.Female:
                     return "Female";
                 default:
                     return "Invalid data detected";
