96cb3d22dedc701ba2f8ef368236ae3b09b4ea1c
imj <imj.programming.2013@gmail.com>
2021-08-17 16:58:30 +1000

    Part 48   C# Tutorial   Difference between Types and Type Members

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -7,67 +7,60 @@ using ProjectA.TeamB;
 using System.IO;
 
  
+
 namespace pragrimtech_c_sharp__vs_2019_
 {
 
+    /// <summary>
+    /// classes, structs, enums, interfaces, delegates are called types
+    /// fields, properties, constructors, methods etc are called type members
+    /// 
+    /// </summary>
+    public class Customer
+    {
+        #region Fields
+        private int _id;
+        private string _firstName;
+        private int _lastName;
+        #endregion
+
+        #region Properties
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public int LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        #endregion
+
+        #region Methods
+        public string GetFullName()
+        {
+            return this._firstName + " " + this._lastName;
+        }
+        #endregion  
+    }
+
     public class Program
     {
-        /// <summary>
-        /// enums are value types 
-        /// an enum is a strongly typed constant
-        /// enum by default has underlying data type of integer starting at 0
-        /// </summary>
+
         public static void Main()
         {
-             //type cast to int array
-            //int[] values = (int[])Enum.GetValues(typeof(Gender));
-             //Enum class contains static methods to operate on enumerations
-            short[] values = (short[])Enum.GetValues(typeof(Gender));
-
-            foreach (var item in values)
-            {
-                Console.WriteLine(item);
-            }
-
-            string[] names = ((string[])Enum.GetNames(typeof(Gender)));
-
-            foreach (var item in names)
-            {
-                Console.WriteLine(item);
-            }
-
-            // though the underlying type is int a type cast or converstion is required
-            //Gender gender = (Gender)3;
-            int number = (int)Gender.Unkown;
-
-            Gender gender = (Gender)Season.Winter;
-            Console.WriteLine(gender);
+             
 
         }
 
-    }
-
-    //public enum Gender
-
-    // : to set underlying type
-    //public enum Gender
-    public enum Gender : short
-    {
-        //Unkown,
-        // will start at 1 instead of 0 or set to any value
-        //Unkown = 1,
-        //Male = 3,
-        //Female = 23
-        Unkown = 1,
-        Male = 2,
-        Female = 3
-    }
-
-    public enum Season
-    {
-        Winter = 1,
-        Spring = 2,
-        Summer = 3
     }
 
 }
