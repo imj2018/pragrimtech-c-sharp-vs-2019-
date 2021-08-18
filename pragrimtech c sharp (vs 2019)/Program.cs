282e3e7ecef1b07416df3ea16ef17df6b859de07
imj <imj.programming.2013@gmail.com>
2021-08-18 22:21:45 +1000

    Part 58 C# Tutorial Why should you override Equals Method

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -17,35 +17,61 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         public static void Main()
         {
-            int Number = 10;
-            Console.WriteLine(Number.ToString());
+            //int i = 10;
+            //int j = 10;
 
-            Customer C1 = new Customer()
-            {
-                FirstName = "Simon",
-                LastName = "Tan"
-            };
+            // System.Object all types in .NET directly or indirectly inherit
+            //Console.WriteLine(i == j);
+            //Console.WriteLine(i.Equals(j));
 
-            Console.WriteLine(C1.ToString());
 
-            // alternatively the convert class
-            Console.WriteLine(Convert.ToString(C1));
+            Direction direction1 = Direction.East;
+            Direction direction2 = Direction.West;
 
+            //Console.WriteLine(direction1 == direction2);
+            //Console.WriteLine(direction1.Equals(direction2));
+
+
+            Customer C1 = new Customer();
+            C1.FirstName = "Simon";
+            C1.LastName = "Tan";
+
+            //Customer C2 = C1;
+            //C2.LastName = "poo";
+
+
+            Customer C2 = new Customer();
+            C2.FirstName = "Simon";
+            C2.LastName = "Tan";
+
+            // value equality is the same but reference equality (reference variable) is
+            // different, it will return false as they are referencing different objects
+            Console.WriteLine("reference equality = {0}", C1 == C2);
+            Console.WriteLine("value equality (overriden Equals method) = {0}", C1.Equals(C2));
 
 
             // messing around
             //if (Calculate.AreEqual<int>(10, 10))
             if (Calculate<int>.AreEqual(10, 10))
             {
-                Console.WriteLine("Equal");
+                //Console.WriteLine("Equal");
             }
             else
             {
-                Console.WriteLine("Not Equal");
+                //Console.WriteLine("Not Equal");
             }
-        }
+         }
 
     }
+
+    public enum Direction
+    {
+        East = 1,
+        West = 2,
+        North = 3,
+        South = 4
+    }
+
 
     public class Customer
     {
@@ -56,6 +82,32 @@ namespace pragrimtech_c_sharp__vs_2019_
         {
             return this.LastName + ", " + this.FirstName;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // needs to check if a Customer or derived/inherited from Customer
+            if (!(obj is Customer))
+            {
+                return false;
+            }
+
+            // check for this instance of FirstName or LastName
+            return 
+                this.FirstName == ((Customer)obj).FirstName &&
+                this.LastName == ((Customer)obj).LastName;
+
+        }
+
+        public override int GetHashCode()
+        {
+            return this.FirstName.GetHashCode() ^ this.LastName.GetHashCode();
+        }
+
     }
 
 
