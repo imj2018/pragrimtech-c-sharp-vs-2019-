04b8c2d96ea0d2208d74d1a6ac2799fe3b87842c
imj <imj.programming.2013@gmail.com>
2021-08-18 20:30:59 +1000

    Part 55   C# Tutorial   Late binding using reflection

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -8,11 +8,8 @@ using System.IO;
 using System.Reflection;
 
 /// <summary>
-/// inspect an assembly's contents by lopking at the meta data at runtime
-/// is reflection
-///  
-/// late binding is creating an instance of a class at runtime rather than
-/// compile time, this can be done using reflection
+/// 
+/// 
 /// </summary>
 namespace pragrimtech_c_sharp__vs_2019_
 {
@@ -21,92 +18,53 @@ namespace pragrimtech_c_sharp__vs_2019_
 
         public static void Main()
         {
-            // we have knowledge of the Customer class at compile time like so
-            // i.e early binding, but there is a possibility that you may
-            // not know the class at runtime e.g assembly is not available.
-            // so a class needs to be created at runtime dynamically i.e late binding
-            // a way to handle this is through reflection
+            // early binding 
+            // errors are detected at compile time
             //Customer C1 = new Customer();
+            //string fullName = C1.GetFullName("Pragim", "Tech");
+            //Console.WriteLine("Full Name = {0}", fullName);
 
-            // using the Type class to get the type of class Customer
-            //Type T = Type.GetType("pragrimtech_c_sharp__vs_2019_.Customer");
+            // late binding, when knowledge of the class/object is unavailable
+            // only use late binding with objects that are not available at compile time
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            
+            // returning/get the type
+            Type customerType = executingAssembly.GetType("pragrimtech_c_sharp__vs_2019_.Customer");
 
-            // can also use tyepof
-            //Type T = typeof(Customer);
+            // Activator will create an instance of the Customer class
+            // object as we don't know what it is
+            // if it were static likely just the type is required
+            object customerInstance = Activator.CreateInstance(customerType);
 
-            // if given the instance of the class instead of the class itself
-            // all objects have GetType inherited from System.Object
-            Customer C1 = new Customer();
-            Type T = C1.GetType();
+            // return an object of type MethodInfo
+            MethodInfo getFullNameMethod = customerType.GetMethod("GetFullName");
 
-            Console.WriteLine("Full Name = {0}", T.FullName);
-            Console.WriteLine("Just the Name = {0}", T.Name);
-            Console.WriteLine("Just the Namespace  = {0}", T.Namespace);
-            Console.WriteLine();
+            string[] parameters = new string[2];
+            parameters[0] = "Pragim";
+            parameters[1] = "Technologies";
 
-            // PropertyInfo and Method info are from System.Reflection
-            Console.WriteLine("Properies in Customers");
-            PropertyInfo[] properties = T.GetProperties();
-            foreach (var property in properties)
-            {
-                //Console.WriteLine(property.Name);
-                Console.WriteLine(property.PropertyType + " " + property.Name);
+            // Invoke needs the class instance/object and parameters
+            // second parameter is expecting an object[] not a string but string
+            // directly/indirectly inherits from object so it can be used
+            //string fullName = (getFullNameMethod.Invoke(customerInstance, parameters).ToString();
+            string fullName = (string)getFullNameMethod.Invoke(customerInstance, parameters);
 
-            }
+            Console.WriteLine("Full Name = {0}", fullName);
 
-            Console.WriteLine();
-            Console.WriteLine("Methods in Customers");
-            MethodInfo[] methods = T.GetMethods();
-            foreach (var method in methods)
-            {
-                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
-
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Constructors in Customers");
-            ConstructorInfo[] constructors = T.GetConstructors();
-            foreach (var constructor in constructors)
-            {
-                //Console.WriteLine(constructor.Name);
-                Console.WriteLine(constructor.ToString());
-
-            }
         }
 
     }
 
     public class Customer
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-
-        public Customer(int ID, string Name)
+        public string GetFullName(string FirstName, string LastName)
         {
-            this.Id = ID;
-            this.Name = Name;
-        }
-         
-        public Customer()
-        {
-            this.Id = -1;
-            this.Name = string.Empty;
-        }
-
-        public void PrintID()
-        {
-            Console.WriteLine("ID = {0}", this.Id);
-        }
-
-        public void PrintName()
-        {
-            Console.WriteLine("Name = {0}", this.Name);
+            return FirstName + " " + LastName;
         }
 
     }
 
- 
+
 }
 
 
