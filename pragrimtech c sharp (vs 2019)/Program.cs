921aa7fedde04394d62a421f428d8b6cedb006c3
imj <imj.programming.2013@gmail.com>
2021-08-18 21:42:15 +1000

    Part 57 C# Tutorial Why should you override ToString Method

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -17,54 +17,58 @@ namespace pragrimtech_c_sharp__vs_2019_
     {
         public static void Main()
         {
-            //bool Equal = Calculator.AreEqual(1, 2);
-            //bool Equal = Calculator.AreEqual("A", "A");
+            int Number = 10;
+            Console.WriteLine(Number.ToString());
 
-            // at runtime boxing and unboxing is occuring as 10 is integer which is a value/struct 
-            // therefore it is cast to a reference type which degrades performance
-            //bool Equal = Calculator.AreEqual(10, 10);
-
-            // can also pass parameters that make no sense
-            //bool Equal = Calculator.AreEqual(10, "AB");
-
-            // "I want this method to operate on the string type"
-            //bool Equal = Calculator.AreEqual<string>("AB", "AB");
-
-            // no converstion will occur i.e boxing and unboxing
-            //bool Equal = Calculator.AreEqual<int>(10, 10);
-            bool Equal = Calculator<int>.AreEqual(10, 10);
-
-
-            if (Equal)
+            Customer C1 = new Customer()
             {
-                Console.WriteLine("Are Equal");
+                FirstName = "Simon",
+                LastName = "Tan"
+            };
+
+            Console.WriteLine(C1.ToString());
+
+            // alternatively the convert class
+            Console.WriteLine(Convert.ToString(C1));
+
+
+
+            // messing around
+            //if (Calculate.AreEqual<int>(10, 10))
+            if (Calculate<int>.AreEqual(10, 10))
+            {
+                Console.WriteLine("Equal");
             }
             else
             {
                 Console.WriteLine("Not Equal");
             }
-
         }
 
     }
 
-    //public class Calculator
-
-    // alternatively make class generic
-    public class Calculator<T>
+    public class Customer
     {
-        //public static bool AreEqual(int Value1, int Value2)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-        // int, string etc directly or indirectly inherit from object in .NET
-        // not longer strongly typed
-        //public static bool AreEqual(object Value1, object Value2)
-
-        // generics, type independant
-        //public static bool AreEqual<T>(T Value1, T Value2)
-        public static bool AreEqual(T Value1, T Value2)
+        public override string ToString()
         {
-            //return Value1 == Value2;
-            return Value1.Equals(Value2);
+            return this.LastName + ", " + this.FirstName;
+        }
+    }
+
+
+
+    //public class Calculate
+    public class Calculate<T>
+    {
+        //public static bool AreEqual<T>(T value1, T value2 )
+
+        public static bool AreEqual(T value1, T value2)
+        {
+            return value1.Equals(value2);
+
         }
 
     }
