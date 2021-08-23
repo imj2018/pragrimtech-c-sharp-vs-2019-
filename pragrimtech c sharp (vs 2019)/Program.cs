a9e83136a7c9dce1ded8e51d2478dfe1e4b93c76
imj <imj.programming.2013@gmail.com>
2021-08-23 22:56:59 +1000

    Part 78   Sort a list of complex types in c#

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 
 /// <summary>
 /// 
@@ -78,91 +79,79 @@ namespace pragrimtech_c_sharp__vs_2019_
                 customer5
             };
 
-            List<int> numbers = new List<int>() { 1, 8, 7, 5, 2, 3, 4, 9, 6 };
 
-            Console.WriteLine("Numbers before sorting");
-            foreach (int number in numbers)
+            Console.WriteLine("Before Sorting");
+            foreach (Customer customer in customers)
             {
-                Console.WriteLine(number);
+                Console.WriteLine(customer.Name + " " + customer.Salary); 
             }
-
-            numbers.Sort();
-
-            Console.WriteLine("Numbers after sorting");
-            foreach (int number in numbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            numbers.Reverse();
-
-            Console.WriteLine("Numbers in descending order");
-            foreach (int number in numbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            List<string> alphabet = new List<string>() { "B", "F", "D", "E", "A", "C" };
-
-            Console.WriteLine("Alphabet before sorting");
-            foreach (string letter in alphabet)
-            {
-                Console.WriteLine(letter);
-            }
-
-            alphabet.Sort();
-
-            Console.WriteLine("Alphabet after sorting");
-            foreach (string letter in alphabet)
-            {
-                Console.WriteLine(letter);
-            }
-
-            alphabet.Reverse();
-
-            Console.WriteLine("Alphabet in descending order");
-            foreach (string letter in alphabet)
-            {
-                Console.WriteLine(letter);
-            }
+            Console.WriteLine("");
 
 
-            //  attempting to use Sort on custom/complex types (Customer List)
-            //  will throw an exception as .NET does not know how to sort complex types
-            //  .NET needs to be told how to sort them at run time
-            //
-            //  simple types already implement IComparable and have a CompareTo method
-            //  within the .NET framework
-            // 
             customers.Sort();
+            customers.Reverse();
+
+
+            Console.WriteLine("After Sorting");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Name + " " + customer.Salary);
+            }
+            Console.WriteLine("");
+
+
+            SortByName sortByName = new SortByName();
+
+            //  if no argument is passed by default the Sort method
+            //  from the class wil be used. but you can provide your own sort logic
+            //  i.e SortByName class can be implemented then passed as an
+            //  argument as it implements IComparer
+            customers.Sort(sortByName);
+
+            Console.WriteLine("Sorting by Name");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+            Console.WriteLine("");
 
 
 
 
 
+            //  messing around with List to Dictionary
+            customers.AddRange(customersCorporate);
 
+            Dictionary<int, Customer> customerDictionary = 
+                customers.ToDictionary(c => c.ID);
 
-
+            foreach (KeyValuePair<int,Customer> keyValuePair in customerDictionary)
+            {
+                Customer customer = keyValuePair.Value;
+                Console.WriteLine(customer.Name);
+            }
+            
 
         }
 
     }
 
-    public enum Names
+
+    //  in real time you may not own the class code but you need to change
+    //  the default Sort functionality, IComparer can be used which has a Compare method
+    //
+    public class SortByName : IComparer<Customer>
     {
-        Agent47 = 101,
-        IllidanStormrage = 102,
-        MasterChief = 103,
-        Warden = 104
+        public int Compare(Customer x, Customer y)
+        {
+            //  Name is string so CompareTo can be used to return an int
+            return x.Name.CompareTo(y.Name);
+        }
     }
 
-
-    public class SavingsCustomer : Customer
-    {
-
-    }
-
-    public class Customer
+    //  implement IComparableInterface
+    //
+    public class Customer : IComparable<Customer>
     {
         public int ID { get; set; }
         public string Name { get; set; }
@@ -170,6 +159,38 @@ namespace pragrimtech_c_sharp__vs_2019_
 
         public string Type { get; set; }
 
+        //  compare the instance of this class to the object passed with CompareTo
+        //  
+        //  if the number returned is > 0 than the instance is greater than the object
+        //  compared with
+        //  if < 0 than object is less, if 0 than equal
+        //
+        public int CompareTo(Customer other)
+        {
+            //if (this.Salary > other.Salary)
+            //{
+            //    return 1;
+            //}
+            //else if (this.Salary < other.Salary)
+            //{
+            //    return -1;
+            //}
+            //else
+            //{
+            //    return 0;
+            //}
+
+            //  as int already has a CompareTo the if check is not needed
+            //  and you can just compare to the Customer object salary passed
+            // 
+            //return this.Salary.CompareTo(other.Salary);
+
+            //  string also has CompareTo so Name can be used
+            //
+            //return this.Name.CompareTo(other.Name);
+
+            return this.Salary.CompareTo(other.Salary);
+        }
 
         #region
         public string FirstName { get; set; }
@@ -216,11 +237,30 @@ namespace pragrimtech_c_sharp__vs_2019_
             Console.WriteLine("Sum = " + result);
 
         }
+
+
         #endregion
 
+        public enum Names
+        {
+            Agent47 = 101,
+            IllidanStormrage = 102,
+            MasterChief = 103,
+            Warden = 104
+        }
+
+
+        public class SavingsCustomer : Customer
+        {
+
+        }
+
+
+
+
+
+
     }
-
-
 
 
 
