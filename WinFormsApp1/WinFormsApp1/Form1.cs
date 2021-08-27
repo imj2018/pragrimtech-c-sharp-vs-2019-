3faadc3cf05b1c9f18af053146f6f9b94b4f2196
imj <imj.programming.2013@gmail.com>
2021-08-27 19:33:55 +1000

    Part 86   Multithreading in C#

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/// <summary>
+///  unlikely to use any time soon, relearn and comment when needed
+/// 
+/// 
+/// </summary>
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTimeConsumingWork_Click(object sender, EventArgs e)
+        {
+            btnTimeConsumingWork.Enabled = false;
+            btnPrintNumbers.Enabled = false;
+
+            Thread workerThread = new Thread(DoTimeConsumingWork);
+            workerThread.Start();
+            //DoTimeConsumingWork();
+
+            btnTimeConsumingWork.Enabled = true;
+            btnPrintNumbers.Enabled = true;
+
+
+        }
+
+        private void btnPrintNumbers_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void DoTimeConsumingWork()
+        {
+            Thread.Sleep(5000);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
