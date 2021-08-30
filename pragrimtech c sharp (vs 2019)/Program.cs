0cda3ff566f1d6c9c888bfcb0b03859bcdebf6de
imj <imj.programming.2013@gmail.com>
2021-08-30 10:14:59 +1000

    Part 95   Deadlock in a multithreaded program

--- pragrimtech c sharp (vs 2019)/Program.cs
+++ pragrimtech c sharp (vs 2019)/Program.cs
@@ -27,31 +27,27 @@ namespace pragrimtech_c_sharp__vs_2019_
         static int Total = 0;
         public static void Main()
         {
+            Console.WriteLine("Main Started");
+            Account accountA = new Account(101, 5000);
+            Account accountB = new Account(102, 3000);
 
-            //AddOneMillion();
-            //AddOneMillion();
-            //AddOneMillion();
+            AccountManager accountManagerA = 
+                new AccountManager(accountA, accountB, 1000);
+            Thread T1 = new Thread(accountManagerA.Transfer);
+            T1.Name = "T1";
 
-            Stopwatch stopWatch = Stopwatch.StartNew();
-            Thread T1 = new Thread(AddOneMillion);
-            Thread T2 = new Thread(AddOneMillion);
-            Thread T3 = new Thread(AddOneMillion);
+            AccountManager accountManagerB =
+                new AccountManager(accountB, accountA, 2000);
+            Thread T2 = new Thread(accountManagerB.Transfer);
+            T2.Name = "T2";
 
             T1.Start();
             T2.Start();
-            T3.Start();
 
             T1.Join();
             T2.Join();
-            T3.Join();
+            Console.WriteLine("Main Completed");
 
-
-            Console.WriteLine("Total = " + Total);
-
-            stopWatch.Stop();
-            Console.WriteLine(stopWatch.ElapsedTicks);
-
-            //Console.WriteLine(TimeSpan.);
 
         }
 
@@ -80,7 +76,77 @@ namespace pragrimtech_c_sharp__vs_2019_
         }
     }
 
+    public class AccountManager
+    {
+        Account _fromAccount;
+        Account _toAccount;
+        double _amountToTransfer;
 
+        public AccountManager( Account fromAccount, Account toAccount, 
+            double amountToTransfer)
+        {
+            this._fromAccount = fromAccount;
+            this._toAccount = toAccount;
+            this._amountToTransfer = amountToTransfer;
+
+        }
+        public void Transfer()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " trying to acquire lock on " +
+                _fromAccount.ID.ToString());
+
+            lock (_fromAccount)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " acquire lock on " +
+                    _fromAccount.ID.ToString());
+
+                Console.WriteLine(Thread.CurrentThread.Name + " suspend for 1 second");
+                Thread.Sleep(1000);
+
+                Console.WriteLine(Thread.CurrentThread.Name + " back in action and trying to acquire lock on " +
+                    _toAccount.ID.ToString());
+
+                lock (_toAccount)
+                {
+                    Console.WriteLine("This code will not be executed");
+                    _fromAccount.Withdraw(_amountToTransfer);
+                    _toAccount.Deposit(_amountToTransfer);
+
+                }
+            }
+        }
+    }
+
+    public class Account
+    {
+        double _balance;
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
+        public Account(int id, double balance)
+        {
+            this._id = id;
+            this._balance = balance;
+
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+    }
 
 
     public class Number
@@ -97,6 +163,7 @@ namespace pragrimtech_c_sharp__vs_2019_
 
         }
     }
+
 
     public class Country
     {
